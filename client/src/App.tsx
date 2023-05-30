01aae401d3e9d8831442006be730f7fc71ddcce1
Marcin Skic <marcinskicoficjal@gmail.com>
2023-05-30 22:29:05 +0200

    Create simple front that can import CSV

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import coronaLogo from "/corona.svg";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+//import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [isImportingVaccinations, setIsImportingVaccinations] =
+        useState(false);
+    const [isImportingCases, setIsImportingCases] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    async function importCases() {
+        setIsImportingCases(true);
+
+        const result = await toast
+            .promise(axios.put("http://localhost:8000/api/import/cases"), {
+                pending: "Importing cases...",
+                success: "Imported succesfully!",
+                error: "Import failed",
+            })
+            .catch();
+
+        console.log(result.data);
+
+        setIsImportingCases(false);
+    }
+
+    async function importVaccinations() {
+        setIsImportingVaccinations(true);
+
+        const result = await axios.put(
+            "http://localhost:8000/api/import/cases"
+        );
+
+        setIsImportingVaccinations(false);
+    }
+
+    return (
+        <>
+            <header style={{ display: "flex", gap: "20px" }}>
+                <h1>Covid visualizer</h1>
+                <img width="50px" src={coronaLogo} alt="Vite" />
+            </header>
+            <button
+                onClick={importCases}
+                disabled={isImportingCases || isImportingVaccinations}
+                className="button"
+                style={{
+                    color: "aliceblue",
+                    backgroundColor: isImportingCases ? "#a1a1aa" : "#0284c7",
+                    border: "none",
+                    padding: "10px",
+                    borderRadius: "5px",
+                }}
+            >
+                Import Cases
+            </button>
+            <br />
+            <hr />
+            <button
+                onClick={importVaccinations}
+                disabled={isImportingCases || isImportingVaccinations}
+                className="button"
+                style={{
+                    color: "aliceblue",
+                    backgroundColor: isImportingVaccinations
+                        ? "#a1a1aa"
+                        : "#0284c7",
+                    border: "none",
+                    padding: "10px",
+                    borderRadius: "5px",
+                }}
+            >
+                Import Vaccinations
+            </button>
+            <ToastContainer position={toast.POSITION.BOTTOM_CENTER} />
+        </>
+    );
 }
 
-export default App
+export default App;
