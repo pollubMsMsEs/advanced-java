2e2d4317b3e5dce52b68c70e2faaa06562c63ee4
EvaSolarska <116517774+EvaSolarska@users.noreply.github.com>
2023-06-01 17:52:43 +0200

    Add country load and test charts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,222 +1,214 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import coronaLogo from "/corona.svg";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Line } from "react-chartjs-2";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
 } from "chart.js";
 import failPromise from "./scripts/failPromise";
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 async function sendImportRequest(url: string) {
-    const toastId = toast.loading("Importing...");
+  const toastId = toast.loading("Importing...");
 
-    try {
-        const result = await axios.put(url);
-        console.log(result);
+  try {
+    const result = await axios.put(url);
+    console.log(result);
 
-        if (result.data.error) {
-            throw new Error(result.data.msg);
-        }
-
-        toast.update(toastId, {
-            render: "Import succedded!",
-            type: "success",
-            isLoading: false,
-            autoClose: 4000,
-        });
-    } catch (error: any) {
-        toast.update(toastId, {
-            render: `Import failed: ${error.message ?? ""}`,
-            type: "error",
-            isLoading: false,
-            autoClose: 4000,
-        });
+    if (result.data.error) {
+      throw new Error(result.data.msg);
     }
+
+    toast.update(toastId, {
+      render: "Import succedded!",
+      type: "success",
+      isLoading: false,
+      autoClose: 4000,
+    });
+  } catch (error: any) {
+    toast.update(toastId, {
+      render: `Import failed: ${error.message ?? ""}`,
+      type: "error",
+      isLoading: false,
+      autoClose: 4000,
+    });
+  }
 }
 
 function App() {
-    const [isImportingVaccinations, setIsImportingVaccinations] =
-        useState(false);
-    const [isImportingCases, setIsImportingCases] = useState(false);
+  const [isImportingVaccinations, setIsImportingVaccinations] = useState(false);
+  const [isImportingCases, setIsImportingCases] = useState(false);
 
-    const [countryList, setCountryList] = useState<string[] | null>([]);
+  const [countryList, setCountryList] = useState<string[] | null>(null);
+  const [chartData, setChartData] = useState<any | null>(null);
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart",
+      },
+    },
+  };
 
-    const data = {
-        labels: ["label 1", "label 2", "label 3"],
+  async function importCases() {
+    setIsImportingCases(true);
+
+    await sendImportRequest("http://localhost:8000/api/import/cases");
+
+    setIsImportingCases(false);
+  }
+
+  async function importVaccinations() {
+    setIsImportingVaccinations(true);
+
+    await sendImportRequest("http://localhost:8000/api/import/vaccinations");
+
+    setIsImportingVaccinations(false);
+  }
+
+  async function getCountriesList() {
+    try {
+      const countriesObj = await axios.get(
+        "http://localhost:8000/api/countries"
+      );
+      const countryNames = Object.keys(countriesObj.data.data);
+      const labels = countryNames.map((country) => `${country}`);
+      const data = {
+        labels: labels,
         datasets: [
-            {
-                label: "data 1",
-                data: [5, 12, 7],
-                borderColor: `#f0f257`,
-                backgroundColor: `#f0f257`,
-            },
+          {
+            label: "data 1",
+            data: [5, 12, 7, 7, 6, 5, 4, 3, 43, 243, 43, 23, 4],
+            borderColor: `#f0f257`,
+            backgroundColor: `#f0f257`,
+          },
         ],
-    };
-
-    const options = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: "top" as const,
-            },
-            title: {
-                display: true,
-                text: "Chart",
-            },
-        },
-    };
-
-    async function importCases() {
-        setIsImportingCases(true);
-
-        await sendImportRequest("http://localhost:8000/api/import/cases");
-
-        setIsImportingCases(false);
+      };
+      setCountryList(countryNames);
+      setChartData(data);
+    } catch (error) {
+      console.log(error);
+      setCountryList(null);
     }
+  }
 
-    async function importVaccinations() {
-        setIsImportingVaccinations(true);
+  useEffect(() => {
+    getCountriesList();
+  }, []);
 
-        await sendImportRequest(
-            "http://localhost:8000/api/import/vaccinations"
-        );
-
-        setIsImportingVaccinations(false);
-    }
-
-    async function getCountriesList() {
-        try {
-            const countriesObj = await axios.get(
-                "http://localhost:8000/api/countries"
-            );
-
-            const temp: string[] = [];
-            for (const [name, _] of countriesObj.data) {
-                temp.push(name);
-            }
-            setCountryList(temp);
-        } catch (error) {
-            setCountryList(null);
-        }
-    }
-
-    return (
+  return (
+    <div
+      style={{
+        display: "grid",
+        height: "100vh",
+        gridTemplateColumns: "250px 1fr",
+      }}
+    >
+      <aside
+        className="aside"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        <header style={{ display: "flex", gap: "20px" }}>
+          <h1>Covid visualizer</h1>
+          <img width="50px" src={coronaLogo} alt="Vite" />
+        </header>
         <div
-            style={{
-                display: "grid",
-                height: "100vh",
-                gridTemplateColumns: "250px 1fr",
-            }}
+          style={{
+            alignSelf: "stretch",
+            marginTop: "-20px",
+          }}
         >
-            <aside
-                className="aside"
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    gap: "10px",
-                }}
-            >
-                <header style={{ display: "flex", gap: "20px" }}>
-                    <h1>Covid visualizer</h1>
-                    <img width="50px" src={coronaLogo} alt="Vite" />
-                </header>
-                <div
-                    style={{
-                        alignSelf: "stretch",
-                        marginTop: "-20px",
-                    }}
-                >
-                    <hr />
-                </div>
-                <button
-                    onClick={importCases}
-                    disabled={isImportingCases || isImportingVaccinations}
-                    className="button"
-                    style={{
-                        color: "aliceblue",
-                        backgroundColor: isImportingCases
-                            ? "#a1a1aa"
-                            : "#0284c7",
-                        border: "none",
-                        padding: "10px",
-                        borderRadius: "5px",
-                    }}
-                >
-                    Import Cases
-                </button>
-
-                <button
-                    onClick={importVaccinations}
-                    disabled={isImportingCases || isImportingVaccinations}
-                    className="button"
-                    style={{
-                        color: "aliceblue",
-                        backgroundColor: isImportingVaccinations
-                            ? "#a1a1aa"
-                            : "#0284c7",
-                        border: "none",
-                        padding: "10px",
-                        borderRadius: "5px",
-                    }}
-                >
-                    Import Vaccinations
-                </button>
-                <ToastContainer position={toast.POSITION.BOTTOM_CENTER} />
-            </aside>
-            <main
-                style={{
-                    padding: "10px",
-                    display: "grid",
-                    gridTemplateColumns: "150px 1fr",
-                    gridTemplateRows: "1fr 200px",
-                }}
-            >
-                <div>
-                    {countryList?.forEach((country) => (
-                        <div>
-                            <span>{country}</span>
-                            <input type="checkbox" name="" id="" />
-                        </div>
-                    )) ?? "Couldn't load countries"}
-
-                    <div>
-                        <span>Argentyna</span>
-                        <input type="checkbox" name="" id="" />
-                    </div>
-                    <div>
-                        <span>Armenia</span>
-                        <input type="checkbox" name="" id="" />
-                    </div>
-                </div>
-                <div
-                    className="chart-container"
-                    style={{ position: "relative", width: "95%" }} //@Skic Required for charts to scale properly
-                >
-                    <Line data={data} options={options} />
-                </div>
-            </main>
+          <hr />
         </div>
-    );
+        <button
+          onClick={importCases}
+          disabled={isImportingCases || isImportingVaccinations}
+          className="button"
+          style={{
+            color: "aliceblue",
+            backgroundColor: isImportingCases ? "#a1a1aa" : "#0284c7",
+            border: "none",
+            padding: "10px",
+            borderRadius: "5px",
+          }}
+        >
+          Import Cases
+        </button>
+
+        <button
+          onClick={importVaccinations}
+          disabled={isImportingCases || isImportingVaccinations}
+          className="button"
+          style={{
+            color: "aliceblue",
+            backgroundColor: isImportingVaccinations ? "#a1a1aa" : "#0284c7",
+            border: "none",
+            padding: "10px",
+            borderRadius: "5px",
+          }}
+        >
+          Import Vaccinations
+        </button>
+        <ToastContainer position={toast.POSITION.BOTTOM_CENTER} />
+      </aside>
+      <main
+        style={{
+          padding: "10px",
+          display: "grid",
+          gridTemplateColumns: "150px 1fr",
+          gridTemplateRows: "1fr 200px",
+        }}
+      >
+        <div>
+          {countryList
+            ? countryList.map((country) => (
+                <div key={country}>
+                  <span>{country}</span>
+                  <input type="checkbox" name={country} id={country} />
+                </div>
+              ))
+            : "Couldn't load countries"}
+        </div>
+        <div
+          className="chart-container"
+          style={{ position: "relative", width: "95%" }} //@Skic Required for charts to scale properly
+        >
+          {chartData ? (
+            <Line data={chartData} />
+          ) : (
+            <p>Nie można załadować danych wykresu</p>
+          )}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
