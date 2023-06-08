baae9e47eefbabd80ed688ebbbf49503b281fd2d
Marcin Skic <marcinskicoficjal@gmail.com>
2023-06-08 23:43:31 +0200

    Extract CountryList as component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, ChangeEvent } from "react";
+import { useState, ChangeEvent } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -16,11 +16,11 @@ import {
     ChartData,
 } from "chart.js";
 import ImportBar from "./components/ImportBar";
-import CountryCheckbox from "./components/CountryCheckbox";
 import DateRangePicker from "@wojtekmaj/react-daterange-picker";
 import "@wojtekmaj/react-daterange-picker/dist/DateRangePicker.css";
 import "react-calendar/dist/Calendar.css";
 import ChartContainer from "./components/ChartContainer";
+import CountryList from "./components/CountryList";
 ChartJS.register(
     CategoryScale,
     LinearScale,
@@ -32,38 +32,29 @@ ChartJS.register(
     Legend
 );
 
-interface CountryData {
-    id: number;
-    name: string;
-}
-
 function App() {
-    const [countryList, setCountryList] = useState<
-        { id: number; name: string }[] | null
-    >(null);
+    const [chartQuery, setChartQuery] = useState<{
+        begin_date: string;
+        end_date: string;
+        countries: number[];
+    }>({ begin_date: "2021-06-18", end_date: "2021-08-27", countries: [] });
 
-    const [startDate, setStartDate] = useState("2021-06-18"); //DEVTEMP 2020-01-03
-    const [endDate, setEndDate] = useState("2021-08-27"); //DEVTEMP 2023-05-17
-    const [selectedCountries, setSelectedCountries] = useState<number[]>([]);
-    const allCountriesChecked =
-        selectedCountries.length === countryList?.length ?? 0;
     const [selectedOptions, setSelectedOptions] = useState({
         vaccinations: false,
         newCases: true, //DEVTEMP
         deaths: false,
     });
 
-    const [chartQuery, setChartQuery] = useState({});
-
     const [chartData, setChartData] = useState<ChartData<"line"> | null>(null);
-    useEffect(() => {
-        getCountriesList();
-    }, []);
 
     function handleDateChange([startDate, endDate]: any) {
-        setStartDate(dayjs(startDate).format("YYYY-MM-DD"));
-        setEndDate(dayjs(endDate).format("YYYY-MM-DD"));
-        //setChartQuery((prevQuery) => {});
+        setChartQuery((prevQuery) => {
+            return {
+                ...prevQuery,
+                begin_date: dayjs(startDate).format("YYYY-MM-DD"),
+                end_date: dayjs(endDate).format("YYYY-MM-DD"),
+            };
+        });
     }
 
     const handleOptionCheckboxChange = (
@@ -76,6 +67,15 @@ function App() {
         }));
     };
 
+    function handleSelectedCountries(countries: number[]) {
+        setChartQuery((prevQuery) => {
+            return {
+                ...prevQuery,
+                countries,
+            };
+        });
+    }
+
     const handleGenerateData = async () => {
         try {
             const { vaccinations, newCases, deaths } = selectedOptions;
@@ -86,11 +86,7 @@ function App() {
                 const responseDeaths = await axios.get(
                     "http://localhost:80/api/deaths",
                     {
-                        params: {
-                            begin_date: startDate,
-                            end_date: endDate,
-                            countries: selectedCountries,
-                        },
+                        params: chartQuery,
                     }
                 );
                 const deathsData = responseDeaths.data;
@@ -111,11 +107,7 @@ function App() {
                 const responseNewCases = await axios.get(
                     "http://localhost:80/api/cases",
                     {
-                        params: {
-                            begin_date: startDate,
-                            end_date: endDate,
-                            countries: selectedCountries,
-                        },
+                        params: chartQuery,
                     }
                 );
                 const newCasesData = responseNewCases.data;
@@ -136,11 +128,7 @@ function App() {
                 const responseVaccinations = await axios.get(
                     "http://localhost:80/api/vaccinations",
                     {
-                        params: {
-                            begin_date: startDate,
-                            end_date: endDate,
-                            countries: selectedCountries,
-                        },
+                        params: chartQuery,
                     }
                 );
 
@@ -171,56 +159,6 @@ function App() {
         }
     };
 
-    const handleAllCountriesCheckboxChange = ({
-        checked,
-    }: {
-        checked: boolean;
-    }) => {
-        if (checked) {
-            setSelectedCountries(countryList?.map((c) => c.id) ?? []);
-        } else {
-            setSelectedCountries([]);
-        }
-    };
-
-    const handleCountryCheckboxChange = ({
-        id,
-        checked,
-    }: {
-        id: number;
-        checked: boolean;
-    }) => {
-        setSelectedCountries((prevSelectedCountries) => {
-            return checked
-                ? [...prevSelectedCountries, id]
-                : prevSelectedCountries.filter((countryId) => countryId !== id);
-        });
-    };
-
-    async function getCountriesList() {
-        try {
-            const countriesResponse = await axios.get(
-                "http://localhost:80/api/countries"
-            );
-            const countriesObj: { [name: string]: number } =
-                countriesResponse.data.data;
-
-            const countries: CountryData[] = [];
-            for (const [name, id] of Object.entries(countriesObj)) {
-                countries.push({ id: id, name });
-
-                // DEVTEMP
-                if (name === "Poland") {
-                    setSelectedCountries([id]);
-                }
-            }
-            setCountryList(countries);
-        } catch (error) {
-            console.error(error);
-            setCountryList(null);
-        }
-    }
-
     return (
         <>
             <div
@@ -250,34 +188,17 @@ function App() {
                             maxHeight: "100%",
                         }}
                     >
-                        <div style={{ overflowY: "auto", flexShrink: 1 }}>
-                            {countryList && (
-                                <CountryCheckbox
-                                    country={{
-                                        id: -1,
-                                        name: "Whole world",
-                                    }}
-                                    updateSelected={
-                                        handleAllCountriesCheckboxChange
-                                    }
-                                    style={{ fontWeight: "bold" }}
-                                    checked={allCountriesChecked}
-                                />
-                            )}
-                            {countryList?.map((country) => (
-                                <CountryCheckbox
-                                    key={country.id}
-                                    country={country}
-                                    updateSelected={handleCountryCheckboxChange}
-                                    checked={selectedCountries.some(
-                                        (v) => v === country.id
-                                    )}
-                                />
-                            )) ?? "Couldn't load countries"}
-                        </div>
+                        <CountryList
+                            selectedCountries={chartQuery.countries}
+                            handleSelectedCountries={handleSelectedCountries}
+                            style={{ overflowY: "auto", flexShrink: 1 }}
+                        />
                         <DateRangePicker
                             onChange={handleDateChange}
-                            value={[new Date(startDate), new Date(endDate)]}
+                            value={[
+                                new Date(chartQuery.begin_date),
+                                new Date(chartQuery.end_date),
+                            ]}
                         />
                         <h3 style={{ margin: "0" }}>COVID-19 data</h3>
                         <div>
