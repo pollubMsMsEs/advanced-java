417908a35976b35d6a8da4f4ce689efa2531af86
Marcin Skic <marcinskicoficjal@gmail.com>
2023-10-12 13:19:56 +0200

    Setup project and test connection to DB

---
+++ server/src/test/java/com/pollubmsmses/advjava/AdvJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollubmsmses.advjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
