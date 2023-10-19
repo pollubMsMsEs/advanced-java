2c946a73a257d744c957d87ec8ee2b987fce6541
Marcin Skic <marcinskicoficjal@gmail.com>
2023-10-19 22:48:49 +0200

    Add reading country codes 3to2 map from JSON file

--- server/src/main/java/com/pollubmsmses/advjava/services/CountryService.java
+++ server/src/main/java/com/pollubmsmses/advjava/services/CountryService.java
@@ -1,10 +1,13 @@
 package com.pollubmsmses.advjava.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pollubmsmses.advjava.models.CasesPerDay;
 import com.pollubmsmses.advjava.models.Country;
 import com.pollubmsmses.advjava.repositories.CountryRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
@@ -17,11 +20,27 @@ import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class CountryService {
     private final CountryRepository countryRepository;
-
     private final String LOCATIONS_PATH = "/importData/locations.csv";
+    private final String ISO_MAPPING_PATH = "/importData/countryISOMapping.json";
 
+    private Map<String, String> code3to2;
+
+    @PostConstruct
+    public void loadISOMapping() {
+        ObjectMapper mapper = new ObjectMapper();
+        TypeReference<Map<String, String>> typeReference = new TypeReference<Map<String, String>>() {};
+
+        try (InputStream inputStream = CountryService.class.getResourceAsStream(ISO_MAPPING_PATH)) {
+            code3to2 = mapper.readValue(inputStream,typeReference);
+            log.info("Country codes read succesfuly, example: POL=" + code3to2.get("POL"));
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public Map<String, String> getCountriesCSV() {
         Map<String, String> countries = new TreeMap<>();
