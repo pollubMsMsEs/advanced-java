629b2c0fd12ff065e0fbdbbbef39e04808263799
MarcinSkrzypek <73038475+MarcinSkrzypek@users.noreply.github.com>
2023-11-14 06:58:27 +0100

    Add tests for exports and imports

--- server/src/main/java/com/pollubmsmses/advjava/services/files/wrappers/DataWrapper.java
+++ server/src/main/java/com/pollubmsmses/advjava/services/files/wrappers/DataWrapper.java
@@ -6,6 +6,15 @@ import java.util.List;
 
 @Data
 public class DataWrapper {
+    private Long id;
     private List<Case> cases;
     private List<VaccinationWrapper> vaccinations;
+
+    public DataWrapper() {
+    }
+
+    public DataWrapper(List<Case> cases, List<VaccinationWrapper> vaccinations) {
+        this.cases = cases;
+        this.vaccinations = vaccinations;
+    }
 }
